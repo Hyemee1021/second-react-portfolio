b181d7bf543aa40959dd0608d2cd947dee14ba53
Hye Mee Park <hyemee1021@gmail.com>
2023-11-27 09:19:35 +1100

    fixed

--- src/components/works/Works.js
+++ src/components/works/Works.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./works.css";
 import Portfolio1 from "../../assets/project1.png";
-import Portfolio2 from "../../assets/portfolio-2.png";
+import Portfolio2 from "../../assets/project2.png";
 import Portfolio3 from "../../assets/portfolio-3.png";
 import Portfolio4 from "../../assets/portfolio-4.png";
 import Portfolio5 from "../../assets/portfolio-5.png";
@@ -9,7 +9,7 @@ import Portfolio6 from "../../assets/portfolio-6.png";
 const Works = () => {
   return (
     <section id="works">
-      <h2 className="worksTitle">My Portfolios</h2>
+      <h2 className="worksTitle">My Projects</h2>
       <span className="worksDesc">
         Here are my projects I have worked on. Stay tune for new projects...
       </span>
@@ -22,7 +22,15 @@ const Works = () => {
         >
           <img src={Portfolio1} alt="first project" className="worksImg" />
         </a>
-        <img src={Portfolio2} alt="" className="worksImg" />
+        <a
+          className="MyProjects"
+          href="https://transaction-client-seven.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={Portfolio2} alt="" className="worksImg" />
+        </a>
+
         <img src={Portfolio3} alt="" className="worksImg" />
         <img src={Portfolio4} alt="" className="worksImg" />
         <img src={Portfolio5} alt="" className="worksImg" />
